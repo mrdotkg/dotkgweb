a0eb11ceb0dba598ec9dc0f1006e0dfb95277180
Kr <grv.rkg@gmail.com>
2024-07-06 20:35:42 +0530

    for gh pages

--- next.config.js
+++ next.config.js
@@ -60,6 +60,8 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
+    output: "export",
+    basepath: "/dotkg.in",
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
