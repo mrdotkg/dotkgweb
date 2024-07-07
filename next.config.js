bc1dd46960685586e85a3e3b8d84a796cf23cbe0
Kr <grv.rkg@gmail.com>
2024-07-07 11:43:12 +0530

    sass-loader removed for config

--- next.config.js
+++ next.config.js
@@ -91,7 +91,7 @@ module.exports = () => {
       },
       {
         test: /\.css$/,
-        use:['style-loader', 'css-loader', 'sass-loader'],
+        use:['style-loader', 'css-loader'],
       })
 
       return config
